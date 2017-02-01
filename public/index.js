9d317e44cd9be0f1cca1d7098803adbd62450f9c
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-01 07:35:41 -0700

    Clean up unwanted code

--- public/index.js
+++ public/index.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-  getPolls()
-});
-
-var pollSubmit = $('.poll-submit').val()
-var pollInput = $('.poll-input').val()
-
 $('.poll-submit').click(function(e) {
   e.preventDefault()
   var poll = $('.poll-input').val()
@@ -23,50 +16,5 @@ $('.poll-submit').click(function(e) {
       opt_four: opt_four
     }
   })
-    // .then(function(response) {
-    //   $('.poll-list').empty()
-    //   getPolls()
-    //   populateDropdown(poll)
-    // })
+  $('.input' ).val('');
 })
-
-function getPolls() {
-  $.get('/polls', function(polls){
-    polls.forEach(function(poll){
-      $('.poll-list').append(
-        `<div class="poll-container">
-        <div class="poll-name-container">
-        <div class="poll-name inline" >${poll.name}</div>
-        </div>
-        <div class="poll-buttons">
-        <button class="inline poll-sort" onClick='getPop(${poll.id})'>Most Popular Links</button>
-        <button class="inline poll-sort" onClick='getNewest(${poll.id})'>Newest Link</button>
-        </div>
-        </div>
-        <div class="poll" id=${poll.id}></div>
-        <br/>
-        `
-      )
-    })
-  })
-}
-
-function deletePoll(id) {
-  $.get(`/polls/${id}`, function() {
-
-    $.ajax({
-      url: `/polls/${id}`,
-      type: 'delete'
-    })
-    .then(function(response) {
-      $('.poll').empty()
-      getPolls()
-    })
-  })
-}
-
-populateDropdown = (poll) => {
-  $('.poll-select').append(`
-    <option id=${poll.id}>${poll.name}</option>
-    `)
-}
