3739620ab90240f2fd5380a839b9de54098d8487
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-01 11:34:44 -0700

    Listening to port

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ const database = require('knex')(configuration);
 const knex = require('knex')(configuration);
 const helpers = require('./helpers.js')
 const fs = require('fs')
+const socketIo = require('socket.io');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer()
+const server = http.createServer(app)
+                 .listen(port, () => {
+                    console.log(`Listening to port ${port}.`);
+                  });
+
+const io = socketIo(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Poehlster'
 
 app.use(express.static(path.join(__dirname, '/public')))
@@ -25,6 +31,10 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', (request, response) => {
   response.sendfile(__dirname + '/public/creation.html')
 })
+
+app.get('/poll', (req, res) => {
+  res.sendFile(__dirname + '/public/poll.html');
+});
 
 app.get('/polls', (request, response) => {
   helpers.getPolls(response)
@@ -39,6 +49,8 @@ app.post('/polls', (request, response) => {
   helpers.postNewPoll(poll, opt_one, opt_two, opt_three, opt_four, response);
 })
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is runnning on ${app.get('port')}`)
-})
+
+
+io.on('connection', (socket) => {
+  console.log('A user has connected.');
+});
