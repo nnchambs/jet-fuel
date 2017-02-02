e2d2401278809d1850d36710899c01843fb72163
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-01 22:09:19 -0700

    Update auth js functions

--- public/auth.js
+++ public/auth.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  var lock = null;
-  lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+
+  var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
     auth: {
       params: { scope: 'openid email' } //Details: https://auth0.com/docs/scopes
     }
@@ -11,6 +11,11 @@ $(document).ready(function() {
     lock.show();
   });
 
+  $('.btn-logout').click(function(e) {
+    e.preventDefault();
+    logout();
+  })
+
   lock.on("authenticated", function(authResult) {
     lock.getProfile(authResult.idToken, function(error, profile) {
       if (error) {
@@ -18,6 +23,36 @@ $(document).ready(function() {
         return;
       }
       localStorage.setItem('id_token', authResult.idToken);
+      // Display user information
+      show_profile_info(profile);
     });
   });
+
+  //retrieve the profile:
+  var retrieve_profile = function() {
+    var id_token = localStorage.getItem('id_token');
+    if (id_token) {
+      lock.getProfile(id_token, function (err, profile) {
+        if (err) {
+          return alert('There was an error getting the profile: ' + err.message);
+        }
+        // Display user information
+        show_profile_info(profile);
+      });
+    }
+  };
+
+  var show_profile_info = function(profile) {
+     $('.nickname').text(profile.nickname);
+     $('.btn-login').hide();
+     $('.avatar').attr('src', profile.picture).show();
+     $('.btn-logout').show();
+  };
+
+  var logout = function() {
+    localStorage.removeItem('id_token');
+    window.location.href = "/";
+  };
+
+  retrieve_profile();
 });
